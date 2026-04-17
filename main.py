8007a10a4aea940f1214df40e4a097dfe04d3fb4
rziyoda2197 <rziyoda2197@gmail.com>
2026-04-17 16:54:47 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#1-misol
+matn = "   "          # yoki " "
+print(matn.isspace())   # Natija: True
+
+#2-misol
+matn = "SALOM"
+print(matn.isupper())   # Natija: True
+
+#3-misol
+matn = "salom"
+print(matn.islower())   # Natija: True
+
+#4-misol
+matn = "Python"
+print(matn.center(30))  
+# Natija:          Python          
+
+#5-misol
+matn = "Python"
+print(matn.ljust(25))
+# Natija: Python                   
